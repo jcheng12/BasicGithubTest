f28a07a957cb540881ce18b810463a4260981981
Cheng <e440223@us.lmco.com>
2023-01-04 14:15:01 -0800

    Updated main.py to display another message line

--- main.py
+++ main.py
@@ -10,3 +10,5 @@ print('Test Message (dir: ' + os.getcwd() + ')')
 test2.test2_func()
 test3.test3_func()
 test4.test4_func()
+
+print('Test Message: I put this here so I would need to do another commit and push')
